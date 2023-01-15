b1681a831842dd6e0a4e926c6b44bd2579fd9ecb
RockybDeath <mdnvalery@yahoo.com>
2023-01-15 21:02:04 +0300

    Comment for checkers

--- src/main/api/api.js
+++ src/main/api/api.js
@@ -2,6 +2,9 @@ const API_BASE_ADDRESS = 'https://router.project-osrm.org/trip/v1/driving/';
 
 const API_BASE_SETTINGS = '?steps=true&overview=false'
 
+
+//Апи почему-то возвращает странные точки, которые идут через реки, дома и т.д, не очень понимаю с чем связано.
+
 export default class Api {
     static async fetchHaul(parameters) {
 
